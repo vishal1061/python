bd685f47468cdbc40d51ccfb2b4802108ba173b3
Vishal1061 <vishal.bisht1061@gmail.com>
2019-12-20 00:03:49 +0530

    Adding the code

---
+++ Ex-29/ex29.py
@@ -0,0 +1,42 @@
+# Exercise 29: If condition
+
+people = 20
+cats = 30
+dogs = 15
+
+# To check if number of people are less than cats or not. 
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+# To check if number of people are greater than cats or not.
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+# To check if number of people are less than dogs or not.
+
+if people < dogs:
+    print("The world is drooled on!")
+
+# To check if number of people are greater than dogs or not.
+if people > dogs:
+    print("The world is dry!")
+
+# Adding 5 to the number of dogs
+
+dogs += 5
+
+# To check if number of people are greater than or equal to dogs.
+
+if people>= dogs:
+    print("People are greater than or equal to dogs.")
+
+# To check if number of people are less than or equal to dogs.
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+# To check if number of people are  equal to dogs or not.
+
+if people == dogs:
+    print("People are dogs")
